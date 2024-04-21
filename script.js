873146f91c502de4761a53ff96436f8435a2d920
UliaKorolenok <uliakorolenok043@gmail.com>
2024-04-21 18:18:18 +0300

    second block

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ $(document).ready(function () {
     });
     
 });
+
+$(document).ready(function () {
+    if (!localStorage.getItem('cookiePopupDisplayed') || localStorage.getItem('cookiePopupDisplayed') !== getCurrentDate()) {
+        $('#cookiePopup').fadeIn();
+    }
+});
+$('#cookiePopup .close').click(function () {
+    $('#cookiePopup').fadeOut();
+});
+
+$('#acceptCookiesBtn').click(function () {
+    $('#cookiePopup').fadeOut();
+    localStorage.setItem('cookiePopupDisplayed', getCurrentDate());
+});
+
+function getCurrentDate() {
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = String(today.getMonth() + 1).padStart(2, '0');
+    var day = String(today.getDate()).padStart(2, '0');
+    return year + '-' + month + '-' + day;
+}
